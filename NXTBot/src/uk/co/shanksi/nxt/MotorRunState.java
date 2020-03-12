1fd108e034a27edef6239d0166adf31d10eba579
shanksi <i.shanks@gmail.com>
2020-03-12 14:52:41 +0000

    Remove old project

--- NXTBot/src/uk/co/shanksi/nxt/MotorRunState.java
+++
@@ -1,9 +0,0 @@
-package uk.co.shanksi.nxt;
-
-public interface MotorRunState
-{
-	byte MOTOR_RUN_STATE_IDLE = 0x00;
-	byte MOTOR_RUN_STATE_RAMPUP = 0x10;
-	byte MOTOR_RUN_STATE_RUNNING = 0x20;
-	byte MOTOR_RUN_STATE_RAMPDOWN = 0x40;
-}
--- NXTBot/src/uk/co/shanksi/nxt/RegulationMode.java
+++
@@ -1,8 +0,0 @@
-package uk.co.shanksi.nxt;
-
-public interface RegulationMode
-{
-	byte REGULATION_MODE_IDLE = 0x00;
-	byte REGULATION_MODE_MOTOR_SPEED = 0x01;
-	byte REGULATION_MODE_MOTOR_SYNC = 0x02;
-}
